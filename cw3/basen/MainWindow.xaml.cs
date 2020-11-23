2110c82c2dd6b3607e23f46eecd733778c2f4a09
DamianNow4321 <damiannowak4321@gmail.com>
2020-11-23 13:51:04 +0100

    ifelse cwiczenie

---
+++ cw3/basen/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace basen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void cText_Click(object sender, RoutedEventArgs e)
+        {
+            if (enCheckbox.IsChecked == true)
+            {
+                if (label.Text == "Z prawej")
+                {
+                    label.Text = "Z lewej";
+                    label.HorizontalAlignment = HorizontalAlignment.Left;
+                }
+                else
+                {
+                    label.Text = "Z prawej";
+                    label.HorizontalAlignment = HorizontalAlignment.Right;
+
+                }
+            }
+            else
+            {
+                label.Text = "Zmiana tekstu wyłączona";
+            }
+        }
+    }
+}
