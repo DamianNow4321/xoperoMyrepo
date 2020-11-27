593bd472b5b75dfa982ed3aa9954ef14d58fd2eb
DamianNow4321 <74734856+DamianNow4321@users.noreply.github.com>
2020-11-27 12:01:23 +0100

    Elephant

---
+++ ResztaCwiczen/Elephant/Elephant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Elephant
+{
+    class Elephant
+    {
+        public string Name;
+        public int EarSize;
+        public int WhoAmI()
+        {
+         MessageBox.Show("Moje uszy mają "+EarSize+" centymetrów szerokości",Name+" mówi...");
+         return 0;
+        }
+    }
+}
