5a75d88833e73763d2ac77b4596a64393f15aae4
DamianNow4321 <74734856+DamianNow4321@users.noreply.github.com>
2020-12-03 10:52:07 +0100

    Planet Mission

---
+++ ResztaCwiczen/PlanetMission/PlanetMission.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanetMission
+{
+    class PlanetMission
+    {
+        public long RocketFuelPerMile;
+        public long RocketSpeedMPH;
+        public int MilesToPlanet;
+        public long UnitsOfFuelNeeded()
+        {
+            return MilesToPlanet * RocketFuelPerMile;
+        }
+        public int TimeNeeded()
+        {
+          return MilesToPlanet / (int)RocketSpeedMPH;
+        }
+        public string FuelNeeded()
+        {
+            return "Będziesz potrzebować " + MilesToPlanet * RocketFuelPerMile + " Jednostek paliwa żeby tam się dostać. Zajmie ci to " + TimeNeeded() + " godzin.";
+        }
+    }
+}
