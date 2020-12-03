5e8c208031aed83b4a7c796e4dd3e190a09dc96d
DamianNow4321 <74734856+DamianNow4321@users.noreply.github.com>
2020-12-03 11:58:48 +0100

    Basen Str365

---
+++ ResztaCwiczen/Basen_S365/Clowns.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basen_S365
+{
+    class Clowns:Picasso
+    {
+        public Clowns() : base("Clowns") { }
+    }
+}
