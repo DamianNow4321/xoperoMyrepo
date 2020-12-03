092bfcd96990cb20092e6ab0e8d17324861091a1
DamianNow4321 <74734856+DamianNow4321@users.noreply.github.com>
2020-12-03 08:59:38 +0100

    Interfejsy

---
+++ ResztaCwiczen/InterfejsyS336/IClown.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfejsyS336
+{
+    interface IClown
+    {
+        void Honk();
+        string FunnyThingIHave { get; }
+    }
+}
---
+++ ResztaCwiczen/InterfejsyS336/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace InterfejsyS336
+{
+    class Program
+    {
+        class Tallguy:IClown
+        {
+            public string Name;
+            public int Height;
+            public void TalkAboutYourself()
+            {
+                Console.WriteLine("Nazywam się " + Name + " i mam "+ Height + " centymetrów wzrostu");
+            }
+            public string FunnyThingIHave
+            {
+                get { return "duże buty"; }
+            }
+            public void Honk()
+            {
+                Console.WriteLine("Tut tuut!");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Tallguy tallguy = new Tallguy { Height = 100, Name = "Adam" };
+            tallguy.Honk();
+        }
+    }
+}
