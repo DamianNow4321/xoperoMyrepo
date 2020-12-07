c36a42c20ca0de3b8693b054d1561646f49b6a5e
DamianNow4321 <74734856+DamianNow4321@users.noreply.github.com>
2020-12-07 15:20:41 +0100

    Magnesy str 447

---
+++ ResztaCwiczen/MagnesyStreamWriter_Str447/Flobbo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MagnesyStreamWriter_Str447
+{
+    class Flobbo
+    {
+        private string zap;
+        public Flobbo(string zap)
+        {
+            this.zap = zap;
+        }
+        public StreamWriter Snobbo()
+        {
+            return new StreamWriter("ara.txt");
+        }
+        public bool Blobbo(StreamWriter sw)
+        {
+            sw.WriteLine(zap);
+            zap = "zielono-purupurowa";
+            return false;
+        }
+        public bool Blobbo(bool Already, StreamWriter sw)
+        {
+            if (Already)
+            {
+                sw.WriteLine(zap);
+                sw.Close();
+                return false;
+            }
+            else
+            {
+                sw.WriteLine(zap);
+                zap = "czerwono-pomarańczowa";
+                return true;
+            }
+        }
+    }
+}
---
+++ ResztaCwiczen/MagnesyStreamWriter_Str447/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace MagnesyStreamWriter_Str447
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Flobbo f = new Flobbo("niebiesko-zółta");
+            StreamWriter sw = f.Snobbo();
+            f.Blobbo(f.Blobbo(f.Blobbo(sw), sw), sw);
+        }
+    }
+}
