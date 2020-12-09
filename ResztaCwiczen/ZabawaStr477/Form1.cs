ddda0a46c8366ec73ca95b36515414c8608550ad
DamianNow4321 <74734856+DamianNow4321@users.noreply.github.com>
2020-12-09 13:33:53 +0100

    Ćwiczenie Str 477

---
+++ ResztaCwiczen/ZabawaStr477/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZabawaStr477
+{
+    public partial class Form1 : Form
+    {
+        Guy joe;
+        Guy bob;
+        int bank = 100;
+        public Form1()
+        {
+            InitializeComponent();
+            bob = new Guy() { Cash = 100, Name = "Bob" };
+            joe = new Guy() { Cash = 50, Name = "Joe" };
+        }
+        public void UpdateForm()
+        {
+            label1.Text = joe.Name + " ma " + joe.Cash+" zł";
+            label2.Text = bob.Name + " ma " + bob.Cash + " zł";
+            label3.Text = "Bank ma " + bank + " zł";
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (bank >= 10)
+            {
+                bank -= joe.ReceiveCash(10);
+                UpdateForm();
+            }
+            else
+            {
+                MessageBox.Show("Bankowi skończyły się pieniądze");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            bank += bob.GiveCash(5);
+            UpdateForm();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bob.ReceiveCash(joe.GiveCash(10));
+            UpdateForm();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            joe.ReceiveCash(bob.GiveCash(5));
+            UpdateForm();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            using (Stream output = File.Create("Guy_File.dat"))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(output, joe);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            using (Stream input = File.OpenRead("Guy_File.dat"))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                joe = (Guy)formatter.Deserialize(input);
+            }
+            UpdateForm();
+        }
+    }
+}
---
+++ ResztaCwiczen/ZabawaStr477/Guy.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZabawaStr477
+{
+    [Serializable]
+    class Guy
+    {
+        public string Name;
+        public int Cash;
+        public int GiveCash(int amount)
+        {
+            if (amount <= Cash && amount > 0)
+            {
+                Cash -= amount;
+                return amount;
+            }
+            else
+            {
+                MessageBox.Show(
+                 "Nie mam wystarczająco pieniędzy by dać ci" + amount,Name + "  mówi...");
+                return 0;
+            }
+        }
+        public int ReceiveCash(int amount)
+        {
+            if (amount > 0)
+            {
+                Cash += amount;
+                return amount;
+            }
+            else
+            {
+                MessageBox.Show(amount + " to nie jest wystarczająco ",Name + " mówi...");
+                return 0;
+            }
+        }
+
+    }
+}
