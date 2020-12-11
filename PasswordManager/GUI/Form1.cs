0f08d9b6ec0174dad71f68dc970de42e5f072cbd
DamianNow4321 <74734856+DamianNow4321@users.noreply.github.com>
2020-12-11 09:38:40 +0100

    Zabezpieczenie haseł

--- PasswordManager/GUI/Form1.cs
+++ PasswordManager/GUI/Form1.cs
@@ -15,6 +15,7 @@ namespace GUI
 {
     public partial class Form1 : Form
     {
+        bool firstTime = true;
         List<Entry> objEntry = Entry.readFile();
         public Form1()
         {
@@ -34,6 +35,27 @@ namespace GUI
         {
             objEntry=Entry.searchPasswords(textBox4.Text);
             listBox1.DataSource = objEntry;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string MPassC = Entry.loadMasterPassword();
+            string mpass = Entry.Hash(textBox5.Text);
+            if (MPassC == "")
+            {
+                firstTime = true;
+            }
+            if (mpass == MPassC || firstTime == true)
+            {
+                textBox4.Visible = true;
+                listBox1.Visible = true;
+                richTextBox1.Visible = true;
+                button2.Visible = true;
+                textBox1.Visible = true;
+                textBox2.Visible = true;
+                textBox3.Visible = true;
+                button1.Visible = true;
+            }
         }
     }
 }
