7e8a49ec7f862b25283633a04b8a2aac8cf04b28
DamianNow4321 <74734856+DamianNow4321@users.noreply.github.com>
2020-12-11 10:28:32 +0100

    Prosty Edytor Tekstu ćw

---
+++ ResztaCwiczen/EdytorTekstu/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EdytorTekstu
+{
+    public partial class Form1 : Form
+    {
+        string name;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                name = openFileDialog1.FileName;
+                textBox2.Clear();
+                textBox2.Text = File.ReadAllText(name);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string name = saveFileDialog1.FileName;
+                File.WriteAllText(name, textBox2.Text);
+            }
+        }
+    }
+}
