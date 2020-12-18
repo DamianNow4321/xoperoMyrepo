426cf0be68a263e848c4c7e0c4d4987892d68201
DamianNow4321 <74734856+DamianNow4321@users.noreply.github.com>
2020-12-18 09:31:54 +0100

    Zagadka basen str623

---
+++ ResztaCwiczen/BasenStr623/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BasenStr623
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kangaroo joey = new Kangaroo();
+            int koala = joey.Wombat(joey.Wombat(joey.Wombat(1)));
+            try
+            {
+                Console.WriteLine((15 / koala) + " jaj na kilogram");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Witaj bracie!");
+            }
+        }
+    }
+}
