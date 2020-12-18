cfa13800ecb901d381e7c94861ea7235e5173923
DamianNow4321 <74734856+DamianNow4321@users.noreply.github.com>
2020-12-18 09:58:26 +0100

    Magnesy zagadka Str629

---
+++ ResztaCwiczen/MagnesyStr629/ExTestDrive.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MagnesyStr629
+{
+    class ExTestDrive
+    {
+        public static void Zero(string test)
+        {
+            try
+            {
+                Console.Write("t");
+                DoRisky(test);
+                Console.Write("o");
+            }
+            catch (MyException)
+            {
+                Console.Write("a");
+            }
+            finally
+            {
+                Console.Write("w");
+            }
+            Console.Write("s");
+        }
+        static void DoRisky(String t)
+        {
+            Console.Write("h");
+            if (t == "yes")
+            {
+                throw new MyException();
+            }
+            Console.Write("r");
+        }
+    }
+}
---
+++ ResztaCwiczen/MagnesyStr629/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MagnesyStr629
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("when it ");
+            ExTestDrive.Zero("yes");
+            Console.Write(" it ");
+            ExTestDrive.Zero("no");
+            Console.WriteLine(".");
+        }
+    }
+}
