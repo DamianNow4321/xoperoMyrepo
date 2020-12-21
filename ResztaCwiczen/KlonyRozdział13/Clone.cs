4414d47003177726f1091b2305e3a5b22a30863b
DamianNow4321 <74734856+DamianNow4321@users.noreply.github.com>
2020-12-21 13:22:23 +0100

    str 648 zadanie

---
+++ ResztaCwiczen/KlonyRozdział13/Clone.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KlonyRozdział13
+{
+    [Serializable]
+    class Clone : IDisposable
+    {
+        public int Id { get; private set; }
+        public Clone(int Id)
+        {
+            this.Id = Id;
+        }
+        public void Dispose()
+        {
+            MessageBox.Show("Zostałem usunięty","Klon " + Id + " mówi...");
+        }
+        ~Clone()
+        {
+            MessageBox.Show("Aaaaaa! Dopadłeś mnie!","Klon " + Id + " mówi...");
+        }
+    }
+}
