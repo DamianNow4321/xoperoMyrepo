aa7232d65160540cdcdc452abeaf0ae65ddc3ea5
DamianNow4321 <74734856+DamianNow4321@users.noreply.github.com>
2020-12-22 10:35:54 +0100

    cwiczenie Str657

---
+++ ResztaCwiczen/WartosciAReferencjeStr657/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WartosciAReferencjeStr657
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Canine spot = new Canine("Burek", "mops");
+            Canine bob = spot;
+            bob.Name = "Szarik";
+            bob.Breed = "beagle";
+            spot.Speak();
+            Dog jake = new Dog("Tofik", "pudel");
+            Dog betty = jake;
+            betty.Name = "Becia";
+            betty.Breed = "pitbul";
+            jake.Speak();
+            Console.ReadKey();
+        }
+    }
+}
