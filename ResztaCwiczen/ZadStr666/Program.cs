81339b94139f35a06dba0dfee4421e629a242973
DamianNow4321 <74734856+DamianNow4321@users.noreply.github.com>
2020-12-23 09:22:24 +0100

    zadanie str666

---
+++ ResztaCwiczen/ZadStr666/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ZadStr666
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj datę urodzin: ");
+            string birthday = Console.ReadLine();
+            Console.Write("Podaj wzrost w centymetrach: ");
+            string height = Console.ReadLine();
+            RobustGuy guy = new RobustGuy(birthday, height);
+            Console.WriteLine(guy.ToString());
+            Console.ReadKey();
+        }
+    }
+}
