2d8dfd904ad987cdaef9bfd98ec6c974ff0b1cac
DamianNow4321 <74734856+DamianNow4321@users.noreply.github.com>
2020-12-23 10:34:14 +0100

    Zad str670

---
+++ ResztaCwiczen/RozszerzenieStr672/OrdinaryHuman.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RozszerzenieStr672
+{
+    sealed class OrdinaryHuman
+    {
+        private int age;
+        int weight;
+        public OrdinaryHuman(int weight)
+        {
+            this.weight = weight;
+        }
+        public void GoToWork() {  }
+        public void PayBills() {  }
+    }
+}
