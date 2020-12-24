8a5664b0323b45a6f8be4564fd4ded1002cceda7
DamianNow4321 <74734856+DamianNow4321@users.noreply.github.com>
2020-12-24 13:02:27 +0100

    Basen str703

---
+++ ResztaCwiczen/BasenStr703/Line.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasenStr703
+{
+    class Line
+    {
+        public string[] Words;
+        public int Value;
+        public Line(string[] Words, int Value)
+        {
+            this.Words = Words; this.Value = Value;
+        }
+    }
+}
