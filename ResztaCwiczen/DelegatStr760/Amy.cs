3d66de2dad5f760ee39acfc15feda66e700b964d
DamianNow4321 <74734856+DamianNow4321@users.noreply.github.com>
2020-12-28 15:17:42 +0100

    Delegat Str760

---
+++ ResztaCwiczen/DelegatStr760/Amy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatStr760
+{
+    class Amy
+    {
+        public GetSecretIngredient AmysSecretIngredientMethod
+        {
+            get
+            {
+                return new GetSecretIngredient(AmysSecretIngredient);
+            }
+        }
+        private string AmysSecretIngredient(int amount)
+        {
+            if (amount < 10)
+                return amount.ToString()
+                            + " puszek sardynek -- potrzebujesz więcej";
+            else
+                return amount.ToString() + " puszek sardynek";
+        }
+    }
+}
+
