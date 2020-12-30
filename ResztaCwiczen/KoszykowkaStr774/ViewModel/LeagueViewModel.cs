13188a3b43e1129c9c91fd80ac7ded7487147212
DamianNow4321 <74734856+DamianNow4321@users.noreply.github.com>
2020-12-30 15:08:25 +0100

    Koszykowka ćw str774

---
+++ ResztaCwiczen/KoszykowkaStr774/ViewModel/LeagueViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KoszykowkaStr774.Model;
+using System.Collections.ObjectModel;
+
+namespace KoszykowkaStr774.ViewModel
+{
+    class LeagueViewModel
+    {
+        public RosterViewModel BriansTeam { get; set; }
+        public RosterViewModel JimmysTeam { get; set; }
+
+        public LeagueViewModel()
+        {
+            Roster briansRoster = new Roster("Bomibiarze", GetBomberPlayers());
+            BriansTeam = new RosterViewModel(briansRoster);
+
+            Roster jimmysRoster = new Roster("Wspaniali", GetAmazinPlayers());
+            JimmysTeam = new RosterViewModel(jimmysRoster);
+        }
+
+        private IEnumerable<Player> GetBomberPlayers()
+        {
+            List<Player> bomberPlayers = new List<Player>() {
+                new Player("Damian", 31, true),
+                new Player("Ludwig", 23, true),
+                new Player("Krystian",6, true),
+                new Player("Maciek", 0, true),
+                new Player("Janek", 42, true),
+                new Player("Hubert",32, false),
+                new Player("Ferdek",8, false),
+            };
+            return bomberPlayers;
+        }
+
+        private IEnumerable<Player> GetAmazinPlayers()
+        {
+            List<Player> amazinPlayers = new List<Player>() {
+                new Player("Kuba",42, true),
+                new Player("Heniek",11, true),
+                new Player("Robert",4, true),
+                new Player("Lucek", 18, true),
+                new Player("Kim", 16, true),
+                new Player("Bartek", 23, false),
+                new Player("Edek",21,  false),
+            };
+            return amazinPlayers;
+        }
+    }
+}
+
---
+++ ResztaCwiczen/KoszykowkaStr774/ViewModel/PlayerViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoszykowkaStr774.ViewModel
+{
+    class PlayerViewModel
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+
+        public PlayerViewModel(string name, int number)
+        {
+            Name = name;
+            Number = number;
+        }
+    }
+}
+
