31134cdc5de9ac2a029e2ae02ca4967c4fae080f
DamianNow4321 <74734856+DamianNow4321@users.noreply.github.com>
2020-12-31 12:58:17 +0100

    Animwana Pszczola Str816

---
+++ ResztaCwiczen/AnimowanaPszczolaStr816/View/FlyingBees.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AnimowanaPszczolaStr816.View
+{
+    /// <summary>
+    /// Logika interakcji dla klasy FlyingBee.xaml
+    /// </summary>
+    public partial class FlyingBee : Window
+    {
+        public FlyingBee()
+        {
+            InitializeComponent();
+        }
+    }
+}
