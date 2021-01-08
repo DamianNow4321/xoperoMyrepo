b54f698a22c3112a140f790be739c9e51255c778
DamianNow4321 <74734856+DamianNow4321@users.noreply.github.com>
2021-01-08 13:12:32 +0100

    magnesy str 124

--- PasswordManager/PasswordManagerTests/UnitTest1.cs
+++ PasswordManager/PasswordManagerTests/UnitTest1.cs
@@ -69,7 +69,6 @@ namespace PasswordManagerTests
         {
             Entry.EncryptString("b14ca5898a4e4133bbce2ea2315a1916", "TEST");
             Entry.DecryptString("b14ca5898a4e4133bbce2ea2315a1916", "TEST");
-
         }
     }
 }
---
+++ ResztaCwiczen/MagnesyStr124/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MagnesyStr124
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result ="";
+            int x = 3;
+            while (x > 0)
+            {
+                if (x > 2)
+                {
+                    result = result + "a";
+                }
+                x = x - 1;
+                result = result + "-";
+
+                if (x == 2)
+                {
+                    result = result + "b c";
+                }
+                if (x == 1)
+                {
+                    result = result + "d";
+                    x = x - 1;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
