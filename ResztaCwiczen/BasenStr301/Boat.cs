fc5d499c670a7ca0ef4d9905fcc819885a946a57
DamianNow4321 <74734856+DamianNow4321@users.noreply.github.com>
2021-01-13 09:44:27 +0100

    BasenStr301

---
+++ ResztaCwiczen/BasenStr301/Boat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasenStr301
+{
+    public class Boat
+    {
+        private int length;
+        public void setLength(int len)
+        {
+            length = len;
+        }
+        public int getLength()
+        {
+            return length;
+        }
+        public virtual string move()
+        {
+            return "dryfuje ";
+        }
+    }
+}
---
+++ ResztaCwiczen/BasenStr301/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BasenStr301
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string xyz = "";
+            Boat b1 = new Boat();
+            Sailboat b2 = new Sailboat();
+            Rowboat b3 = new Rowboat();
+            b2.setLength(32);
+            xyz = b1.move();
+            xyz += b3.move();
+            xyz += b2.move();
+            Console.WriteLine(xyz);
+        }
+    }
+}
